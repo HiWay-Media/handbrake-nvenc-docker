a83f5eb2de05f4563d1116c299fff0ed9eaf4b5e
attiliodrei <attilio.drei@gmail.com>
2023-04-11 22:43:13 +0200

    added test

--- test.sh
+++ test.sh
@@ -2,10 +2,10 @@ docker run --rm -it --gpus all \
     -e NVIDIA_VISIBLE_DEVICES=all \
     -e NVIDIA_DRIVER_CAPABILITIES=compute,utility,video \
     --volume $PWD:/tmp \
-    docker-registry.tngrm.io/handbrake:ffmpeg5 \
+    docker-registry.tngrm.io/handbrake:ffmpeg5  ffmpeg \
       -hwaccel_device 0 \
       -hwaccel cuvid \
       -c:v h264_cuvid \
-      -i /tmp/nput.mp4 \
+      -i /tmp/input.mp4 \
       -c:v h264_nvenc \
       /tmp/out.mkv
